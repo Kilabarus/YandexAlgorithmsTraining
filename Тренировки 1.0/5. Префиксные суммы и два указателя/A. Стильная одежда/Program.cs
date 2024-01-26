12e9f4a83801971a17a8dbde397c5301d597ae3a
Kilabarus <kilabarus@gmail.com>
2024-01-26 07:14:40 +0300

    Add problems A-B, edit template for GetInput method

---
+++ Тренировки 1.0/5. Префиксные суммы и два указателя/A. Стильная одежда/Program.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+
+namespace YandexTraining
+{
+    internal class Program
+    {
+        static List<string> GetInput()
+        {
+            const string INPUT_FILE = "input.txt";
+
+            return File.ReadAllLines(INPUT_FILE).ToList();
+        }
+
+        static void WriteOutput(string output)
+        {
+            const string OUTPUT_FILE = "output.txt";
+
+            File.WriteAllText(OUTPUT_FILE, output);
+        }
+
+        static string Solve(List<string> input)
+        {
+            List<int> ConvertStringToListOfInt32(string ints)
+            {
+                return ints.Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                           .Select(int.Parse)
+                           .ToList();
+            }            
+
+            var shirts = ConvertStringToListOfInt32(input[1]);
+            var trousers = ConvertStringToListOfInt32(input[3]);
+
+            int currentShirt = 0;
+            int currentTrousers = 0;
+
+            int minDifference = Math.Abs(shirts[0] - trousers[0]);
+            int bestShirt = 0;
+            int bestTrousers = 0;
+
+            while (currentShirt < shirts.Count && currentTrousers < trousers.Count)
+            {
+                int currentDifference = Math.Abs(shirts[currentShirt] - trousers[currentTrousers]);
+
+                if (currentDifference < minDifference)
+                {
+                    bestShirt = currentShirt;
+                    bestTrousers = currentTrousers;
+
+                    minDifference = currentDifference;
+                }
+
+                if (shirts[currentShirt] < trousers[currentTrousers])
+                {
+                    ++currentShirt;
+                    continue;
+                }
+
+                ++currentTrousers;
+            }
+
+            while (currentShirt < shirts.Count)
+            {
+                int currentDifference = Math.Abs(shirts[currentShirt] - trousers[currentTrousers - 1]);
+
+                if (currentDifference < minDifference)
+                {
+                    bestShirt = currentShirt;
+                    bestTrousers = currentTrousers - 1;
+
+                    minDifference = currentDifference;
+                }
+
+                ++currentShirt;
+            }
+
+            while (currentTrousers < trousers.Count)
+            {
+                int currentDifference = Math.Abs(shirts[currentShirt - 1] - trousers[currentTrousers]);
+
+                if (currentDifference < minDifference)
+                {
+                    bestShirt = currentShirt - 1;
+                    bestTrousers = currentTrousers;
+
+                    minDifference = currentDifference;
+                }
+
+                ++currentTrousers;
+            }
+
+            return $"{shirts[bestShirt]} {trousers[bestTrousers]}";
+        }
+
+        static void Main(string[] args)
+        {
+            string output = Solve(GetInput());
+
+            WriteOutput(output);
+            Console.WriteLine(output);
+        }
+    }
+}
--- Тренировки 1.0/5. Префиксные суммы и два указателя/B. Сумма номеров/Program.cs
+++ Тренировки 1.0/5. Префиксные суммы и два указателя/B. Сумма номеров/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.IO;
+using System.Numerics;
 
 
 namespace YandexTraining
@@ -20,7 +21,7 @@ namespace YandexTraining
 
             List<string> input = new();
             do
-            {
+            {                
                 input.Add(Console.ReadLine());
             } while (input[^1] != "");
 
@@ -45,9 +46,49 @@ namespace YandexTraining
 
             StringBuilder output = new StringBuilder();
 
-            
+            var NK = ConvertStringToListOfInt32(input[0]);
+            BigInteger N = NK[0];
+            BigInteger K = NK[1];
 
-            return output.ToString();
+            var numbers = ConvertStringToListOfInt32(input[1]);
+
+            int left = 0;
+            int right = 0;
+            BigInteger sumBetween = 0;
+            
+            int count = 0;
+            while (right < numbers.Count)
+            {
+                sumBetween += numbers[right];
+                ++right;
+                
+                while (sumBetween > K)
+                {
+                    sumBetween -= numbers[left];
+                    ++left;
+                }                
+                
+                if (sumBetween == K)
+                {
+                    ++count;
+
+                    sumBetween -= numbers[left];
+                    ++left;
+                }                
+            }
+
+            while (sumBetween > K)
+            {
+                sumBetween -= numbers[left];
+                ++left;
+            }
+
+            if (sumBetween == K)
+            {
+                ++count;
+            }
+
+            return count.ToString();
         }
 
         static void Main(string[] args)
