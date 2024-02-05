a6c16575aedb4c945baf9d76bd08c3034eddbe6b
Kilabarus <kilabarus@gmail.com>
2024-02-05 12:32:47 +0300

    Add problems F-G

---
+++ Тренировки 1.0/6. Бинарный поиск/F. Очень легкая задача/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Numerics;
+
+
+namespace YandexTraining
+{
+    internal class Program
+    {
+        const string LOCAL_INPUT_FILE = @"C:\Users\Admin\Desktop\Programming\.NET\Programs\YandexTraining\input.txt";
+        const string SERVER_INPUT_FILE = "input.txt";
+
+        const string LOCAL_OUTPUT_FILE = @"C:\Users\Admin\Desktop\Programming\.NET\Programs\YandexTraining\output.txt";
+        const string SERVER_OUTPUT_FILE = "output.txt";
+
+        static List<string> ReadInput() => File.Exists(LOCAL_INPUT_FILE)
+            ? File.ReadAllLines(LOCAL_INPUT_FILE).ToList()
+            : File.ReadAllLines(SERVER_INPUT_FILE).ToList();
+
+        static void WriteOutput(string output)
+        {
+            string outputFile = File.Exists(LOCAL_OUTPUT_FILE)
+                ? LOCAL_OUTPUT_FILE
+                : SERVER_OUTPUT_FILE;
+
+            File.WriteAllText(outputFile, output);
+        }
+
+        static string Solve(List<string> input)
+        {
+            List<int> ConvertStringToListOfInt32(string ints)
+            {
+                return ints.Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                           .Select(int.Parse)
+                           .ToList();
+            }
+
+            var NXY = ConvertStringToListOfInt32(input[0]);
+
+            (BigInteger N, int x, int y) = (NXY[0], NXY[1], NXY[2]);
+            
+            if (x > y)
+            {
+                (x, y) = (y, x);
+            }
+
+            BigInteger left = 0;
+            BigInteger right = N * x;
+            while (left < right)
+            {
+                BigInteger middle = (left + right) / 2;
+                if (middle / x + middle / y >= N - 1)
+                {
+                    right = middle;
+                    continue;
+                }
+
+                left = middle + 1;
+            }
+
+            return $"{left + x}";
+        }
+
+        static void Main(string[] args)
+        {
+            string output = Solve(ReadInput());
+
+            WriteOutput(output);
+            Console.WriteLine(output);
+        }
+    }
+}
---
+++ Тренировки 1.0/6. Бинарный поиск/G. Площадь/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Numerics;
+
+
+namespace YandexTraining
+{
+    internal class Program
+    {
+        const string LOCAL_INPUT_FILE = @"C:\Users\Admin\Desktop\Programming\.NET\Programs\YandexTraining\input.txt";
+        const string SERVER_INPUT_FILE = "input.txt";
+
+        const string LOCAL_OUTPUT_FILE = @"C:\Users\Admin\Desktop\Programming\.NET\Programs\YandexTraining\output.txt";
+        const string SERVER_OUTPUT_FILE = "output.txt";
+
+        static List<string> ReadInput() => File.Exists(LOCAL_INPUT_FILE)
+            ? File.ReadAllLines(LOCAL_INPUT_FILE).ToList()
+            : File.ReadAllLines(SERVER_INPUT_FILE).ToList();
+
+        static void WriteOutput(string output)
+        {
+            string outputFile = File.Exists(LOCAL_OUTPUT_FILE)
+                ? LOCAL_OUTPUT_FILE
+                : SERVER_OUTPUT_FILE;
+
+            File.WriteAllText(outputFile, output);
+        }
+
+        static string Solve(List<string> input)
+        {
+            BigInteger n = BigInteger.Parse(input[0]);
+            BigInteger m = BigInteger.Parse(input[1]);
+            BigInteger t = BigInteger.Parse(input[2]);
+
+            if (n > m)
+            {
+                (n, m) = (m, n);
+            }
+
+            BigInteger left = 0;
+            BigInteger right = n / 2;
+            while (left < right)
+            {
+                BigInteger middle = (left + right + 1) / 2;
+
+                if (2 * n * middle + 2 * m * middle - 4 * middle * middle <= t)
+                {
+                    left = middle;
+                    continue;
+                }
+
+                right = middle - 1;
+            }
+
+            return $"{left}";
+        }
+
+        static void Main(string[] args)
+        {
+            string output = Solve(ReadInput());
+
+            WriteOutput(output);
+            Console.WriteLine(output);
+        }
+    }
+}
