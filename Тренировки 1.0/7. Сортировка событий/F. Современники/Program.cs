59479a294f5c76b6507e58dacd5c763d5f2b44bb
Kilabarus <kilabarus@gmail.com>
2024-02-14 15:08:46 +0300

    Add problems F-G

---
+++ Тренировки 1.0/7. Сортировка событий/F. Современники/Program.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+
+namespace YandexTraining
+{
+    internal class Program
+    {
+        const string LOCAL_INPUT_FILE = @"C:\Users\Admin\Desktop\Programming\.NET\Programs\YandexTraining\input.txt";
+        const string SERVER_INPUT_FILE = "input.txt";
+
+        const string LOCAL_OUTPUT_FILE = @"C:\Users\Admin\Desktop\Programming\.NET\Programs\YandexTraining\output.txt";
+        const string SERVER_OUTPUT_FILE = "output.txt";
+
+        static List<string> ReadInput() => File.Exists(LOCAL_INPUT_FILE)
+            ? File.ReadAllLines(LOCAL_INPUT_FILE).ToList()
+            : File.ReadAllLines(SERVER_INPUT_FILE).ToList();
+
+        static void WriteOutput(string output)
+        {
+            string outputFile = File.Exists(LOCAL_OUTPUT_FILE)
+                ? LOCAL_OUTPUT_FILE
+                : SERVER_OUTPUT_FILE;
+
+            File.WriteAllText(outputFile, output);
+        }
+
+        enum EventType
+        {
+            Died = 0,
+            Turned18 = 1,
+        }
+
+        struct Event
+        {
+            public EventType Type;
+            public DateOnly Time;
+            public int PersonId;
+        }
+
+        static string Solve(List<string> input)
+        {
+            List<int> ConvertStringToListOfInt32(string ints)
+            {
+                return ints.Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                           .Select(int.Parse)
+                           .ToList();
+            }
+
+            StringBuilder output = new StringBuilder();
+
+            int N = ConvertStringToListOfInt32(input[0])[0];
+
+            int id = 1;
+            List<Event> events = new();
+            foreach (var bdDates in input.Skip(1).Take(N))
+            {
+                var ints = ConvertStringToListOfInt32(bdDates);
+
+                (int bDay, int bMonth, int bYear, int dDay, int dMonth, int dYear)
+                    = (ints[0], ints[1], ints[2], ints[3], ints[4], ints[5]);
+
+                DateOnly bDate = new(bYear, bMonth, bDay);
+                DateOnly dDate = new(dYear, dMonth, dDay);
+
+                DateOnly t18Date = bDate.AddYears(18);
+                DateOnly t80Date = bDate.AddYears(80);
+
+                if (dDate <= t18Date)
+                {
+                    ++id;
+                    continue;
+                }
+
+                events.Add(new Event() { Time = t18Date, Type = EventType.Turned18, PersonId = id });
+
+                dDate = t80Date < dDate ? t80Date : dDate;
+
+                events.Add(new Event() { Time = dDate, Type = EventType.Died, PersonId = id });
+
+                ++id;
+            }
+
+            bool newParticipant = false;
+            HashSet<int> currParticipating = new();
+            List<HashSet<int>> sets = new();
+            foreach (var e in events.OrderBy(e => e.Time).ThenBy(e => e.Type))
+            {
+                if (e.Type == EventType.Turned18)
+                {
+                    currParticipating.Add(e.PersonId);
+                    newParticipant = true;
+                }
+
+                if (e.Type == EventType.Died)
+                {
+                    if (newParticipant)
+                    {
+                        sets.Add(new(currParticipating));
+                        newParticipant = false;
+                    }
+
+                    currParticipating.Remove(e.PersonId);
+                }
+            }
+
+            if (sets.Count == 0)
+            {
+                return "0";
+            }
+
+            foreach (var set in sets)
+            {
+                foreach (var participant in set)
+                {
+                    output.Append($"{participant} ");
+                }
+
+                output.AppendLine();
+            }
+
+            return output.ToString();
+        }
+
+        static void Main(string[] args)
+        {
+            string output = Solve(ReadInput());
+
+            WriteOutput(output);
+            Console.WriteLine(output);
+        }
+    }
+}
---
+++ Тренировки 1.0/7. Сортировка событий/G. Детский праздник/Program.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Numerics;
+using System.Reflection;
+
+
+namespace YandexTraining
+{
+    internal class Program
+    {
+        const string LOCAL_INPUT_FILE = @"C:\Users\Admin\Desktop\Programming\.NET\Programs\YandexTraining\input.txt";
+        const string SERVER_INPUT_FILE = "input.txt";
+
+        const string LOCAL_OUTPUT_FILE = @"C:\Users\Admin\Desktop\Programming\.NET\Programs\YandexTraining\output.txt";
+        const string SERVER_OUTPUT_FILE = "output.txt";
+
+        static List<string> ReadInput() => File.Exists(LOCAL_INPUT_FILE)
+            ? File.ReadAllLines(LOCAL_INPUT_FILE).ToList()
+            : File.ReadAllLines(SERVER_INPUT_FILE).ToList();
+
+        static void WriteOutput(string output)
+        {
+            string outputFile = File.Exists(LOCAL_OUTPUT_FILE)
+                ? LOCAL_OUTPUT_FILE
+                : SERVER_OUTPUT_FILE;
+
+            File.WriteAllText(outputFile, output);
+        }
+
+        static List<int> IsEnoughTime(List<(int T, int Z, int Y)> people, int M, int numOfMinutes)
+        {
+            int sum = 0;
+            bool done = false;
+            List<int> numOfBalloonsPerPerson = new();
+            foreach (var (T, Z, Y) in people)
+            {
+                if (done)
+                {
+                    numOfBalloonsPerPerson.Add(0);
+                    continue;
+                }
+
+                int numOfFullCycles = numOfMinutes / (T * Z + Y);                                
+                int tForLastCycle = numOfMinutes - numOfFullCycles * (T * Z + Y);
+
+                int numOfBalloonsOnLastCycle = tForLastCycle < T * Z
+                    ? tForLastCycle / T
+                    : Z;
+
+                int numOfBalloons = numOfFullCycles * Z + numOfBalloonsOnLastCycle;
+
+                if (sum + numOfBalloons <= M)
+                {
+                    numOfBalloonsPerPerson.Add(numOfBalloons);
+                    sum += numOfBalloons;
+
+                    if (sum == M)
+                    {
+                        done = true;
+                    }
+
+                    continue;
+                }
+
+                numOfBalloons -= sum + numOfBalloons - M;
+                sum = M;
+                numOfBalloonsPerPerson.Add(numOfBalloons);
+                done = true;
+            }
+
+            return sum == M
+                ? numOfBalloonsPerPerson
+                : null;
+        }
+
+        static int BinSearch(List<(int T, int Z, int Y)> people, int M)
+        {
+            int left = 0;
+            int right = 200 * M;
+
+            while (left < right)
+            {
+                int middle = (left + right) / 2;
+
+                var numOfBalloonsPerPerson = IsEnoughTime(people, M, middle);
+                if (numOfBalloonsPerPerson != null)
+                {
+                    right = middle;
+                    continue;
+                }
+
+                left = middle + 1;
+            }
+
+            return left;
+        }
+
+        static string Solve(List<string> input)
+        {
+            List<int> ConvertStringToListOfInt32(string ints)
+            {
+                return ints.Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                           .Select(int.Parse)
+                           .ToList();
+            }
+
+            StringBuilder output = new StringBuilder();
+
+            var MN = ConvertStringToListOfInt32(input[0]);
+            (int M, int N) = (MN[0], MN[1]);
+
+            List<(int T, int Z, int Y)> people = new();
+            foreach (var personStr in input.Skip(1).Take(N))
+            {
+                var TZY = ConvertStringToListOfInt32(personStr);                
+                people.Add((TZY[0], TZY[1], TZY[2]));
+            }
+
+            int neededTime = BinSearch(people, M);
+            var numOfBalloonsPerPerson = IsEnoughTime(people, M, neededTime);
+
+            output.AppendLine($"{neededTime}");
+            numOfBalloonsPerPerson.ForEach(x => output.Append($"{x} "));
+
+            return output.ToString();
+        }
+
+        static void Main(string[] args)
+        {
+            string output = Solve(ReadInput());
+
+            WriteOutput(output);
+            Console.WriteLine(output);
+        }
+    }
+}
